32e9541722bd645b9f5aed32257bc71d72dabef3
oerdbej <oerdbej@gmail.com>
2022-05-02 16:32:53 +0200

    logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
 import ProgressBar from "./components/ProgressBar/ProgressBar";
 
-function App() {
+const App = () => {
   return (
     <div className='App'>
-      <div className='App-div'>
-        <h1>here </h1>
+      <div className='App-header'>
         <ProgressBar />
+        <p>
+          Edit<code>src/App.js</code>and save to reload
+        </p>
+        <a
+          rel='noopener noreferrer'
+          href='https://reactjs.org'
+          className='App-link'
+          target='_blank'
+        >
+          Learn React
+        </a>
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,6 +1,44 @@
-import React from "react";
-const ProgressBar = () => {
-  return <h3>here</h3>;
+import React, { useState, useEffect } from "react";
+
+const ProgressBar = (props) => {
+  //todo 1:
+
+  const { size, progress, strokeWidth, circleOneStroke, circleTwoStroke } =
+    props;
+
+  //todo 3:
+  const center = size / 2;
+
+  //todo 4:
+  const radius = size / 2 - strokeWidth / 2;
+
+  //todo 5:
+  const circumference = 2 * Math.PI * radius;
+
+  //todo 2:(we have input the atribute after step 3)
+  return (
+    <div>
+      <svg width={size} height={size} className='circular-chart'>
+        <circle
+          className='circular-bg stroke={circleOneStroke}'
+          cx={center}
+          cy={center}
+          r={radius}
+          strokeWidth={strokeWidth}
+        ></circle>
+        <circle
+          className='circle'
+          stroke={circleTwoStroke}
+          cx={center}
+          cy={center}
+          r={radius}
+          strokeWidth={strokeWidth}
+          strokeDasharray={circumference}
+          strokeDashoffset={progress}
+        ></circle>
+      </svg>
+    </div>
+  );
 };
 
 export default ProgressBar;
