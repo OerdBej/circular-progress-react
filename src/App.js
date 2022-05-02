450df8b5938c56119c787a3e36a7e36f656ab596
oerdbej <oerdbej@gmail.com>
2022-05-02 19:02:35 +0200

    function mode random button

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
+import React, { useState } from "react";
 import "./App.css";
 import ProgressBar from "./components/ProgressBar/ProgressBar";
 
 const App = () => {
-  //todo 10: setting up the props for ProgressBar. progress as intiger or decimale number.the strokeWidth is the width of the circle
+  //14: setting up the useState in order to make the change from the user
+
+  const [progress, setProgress] = useState(0);
+
+  //15: this is the function from the button that graps the event of the user and then we get the value and update it and using the states. So now the progress in the State props it changes from onChange function to the first state.
+
+  const onChange = (e) => {
+    if (e.target.value) {
+      let progress = parseInt(e.target.value, 10);
+      if (progress > 100) {
+        progress = 100;
+      }
+      setProgress(progress);
+    } else {
+      setProgress(0);
+    }
+  };
+
+  //18: Create the function from the button randomize event.Whenever that is clicked we set up a random value.
+
+  const randomProgressValue = () => {
+    const progressValue = Math.floor(Math.random() * 101);
+    setProgress(progressValue);
+  };
+
+  //10: setting up the props for ProgressBar. progress as intiger or decimale number.the strokeWidth is the width of the circle
 
   const state = {
     size: 250,
-    progress: 40,
+    progress: progress,
+    // progress: 40, => it changes now from onChange method, its settin
     strokeWidth: 15,
     circleOneStroke: "#d9edfe",
     circleTwoStroke: "#7ea9e1",
   };
 
-  //todo11: we are going to pass the object into the component and distructure it. We can do it with the destructure mode or we can do it more "manually". We can use it in this way or in the more old fashioned way and they are totally the same.So we defined it as an object and destructure it in the components and passing it as a state and getting this with props to the other page. So we can change the value inside of the object and we are going to add a new proprety and we can added in this.
+  //11: we are going to pass the object into the component and distructure it. We can do it with the destructure mode or we can do it more "manually". We can use it in this way or in the more old fashioned way and they are totally the same.So we defined it as an object and destructure it in the components and passing it as a state and getting this with props to the other page. So we can change the value inside of the object and we are going to add a new proprety and we can added in this.
 
   return (
     <div className='App'>
       <div className='App-header'>
         <ProgressBar {...state} />
         {/* <ProgressBar size="250" progress={40}/> */}
-        <p>Progress Bar</p>
-        <a
-          rel='noopener noreferrer'
-          href='https://reactjs.org'
-          className='App-link'
-          target='_blank'
-        >
-          Learn React
-        </a>
+        <p>
+          <input
+            type='text'
+            name='percent'
+            placeholder='Add Progress Value'
+            onChange={onChange}
+          />
+        </p>
+
+        <button onClick={randomProgressValue}>Random</button>
       </div>
     </div>
   );
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,42 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import "./ProgressBar.css";
 
 const ProgressBar = (props) => {
-  //todo 7: using the states
+  //7: using the states
 
   const [offset, setOffset] = useState(0);
 
-  //todo 1:
+  //1:
 
   const { size, progress, strokeWidth, circleOneStroke, circleTwoStroke } =
     props;
 
-  //todo 3:
+  //3:
   const center = size / 2;
 
-  //todo 4:
+  //4:
   const radius = size / 2 - strokeWidth / 2;
 
-  //todo 5:
+  //5:
   const circumference = 2 * Math.PI * radius;
 
-  //todo8: using the useEffect. Passing the dependencies because they are defined outside of this function. we are passing also the offset because we need to update it immediately and have the latest value, if we are not going is going to return the default value that is 0. Once its updated it will updated to the new value as well.
+  //8: using the useEffect. Passing the dependencies because they are defined outside of this function. we are passing also the offset because we need to update it immediately and have the latest value, if we are not going is going to return the default value that is 0. Once its updated it will updated to the new value as well.
 
   useEffect(() => {
     const progressOffset = ((100 - progress) / 100) * circumference;
     setOffset(progressOffset);
   }, [setOffset, progress, circumference, offset]);
 
-  //todo 2:(we have input the atribute after step 3)
+  //2:(we have input the atribute after step 3)
   return (
     <div>
-      <svg width={size} height={size} className='circular-chart'>
+      <svg className='circular-chart' width={size} height={size}>
         <circle
-          className='circular-bg stroke={circleOneStroke}'
+          className='circular-bg'
+          stroke={circleOneStroke}
           cx={center}
           cy={center}
           r={radius}
           strokeWidth={strokeWidth}
         ></circle>
+
         <circle
           className='circle'
           stroke={circleTwoStroke}
@@ -44,11 +47,11 @@ const ProgressBar = (props) => {
           cy={center}
           r={radius}
           strokeWidth={strokeWidth}
-          //todo 6: Now we create the states Hooks in order to make the changes in order to get the updated values. we have to use the useEffect in order to change and get the value to the props and then use that value here.
+          //6: Now we create the states Hooks in order to make the changes in order to get the updated values. we have to use the useEffect in order to change and get the value to the props and then use that value here.
           strokeDasharray={circumference}
           strokeDashoffset={offset} //!the progress is empty we get pass the offset from the hook
         ></circle>
-
+        {/* //9: going to display the progress value that it would get */}
         <text x={center} y={center} className='percentange'>
           {progress}%
         </text>
